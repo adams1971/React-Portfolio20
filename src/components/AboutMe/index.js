b1ba402b4bb6b036a979fd40774c005d4b9e1c5f
adam stevenson <adamscdcm@gmail.com>
2021-06-01 21:52:49 -0700

    fixed pic

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Container, Row, Col, Jumbotron } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
+import myImage from '../../assets/Images/headShot1.JPG'
+
+
 // import oceanBG from './oceanBG.jpg';
 // import myImage from './images/headShot1.JPG';
 
@@ -18,7 +21,8 @@ function HomePg() {
                         Welcome to my Landing Page, I hope you like what you see!
                         </h4>
                         
-                        <img className="img-fluid float-left rounded-circle portrait" src="./headShot1.JPG" alt="my head shot" style={{ maxWidth: "25%", maxHeight: "auto" }}/>
+                        {/* <img className="img-fluid float-left rounded-circle portrait" src="./headShot1.JPG" alt="my head shot" style={{ maxWidth: "25%", maxHeight: "auto" }}/> */}
+                        <img className="img-fluid float-left rounded-circle portrait" src={myImage} alt="my head shot" style={{ maxWidth: "25%", maxHeight: "auto" }}/>
                         {/* <br /> */}
                         <h4 style={{ color: '#154360', opacity: '70%', fontSize: '15px'}}>Hi, My name is Adam Stevenson, I have completed a Full Stack Web Developer Bootcamp through the University of Washington. As I continue to develop my skills, I look forward to a full time career in Front End Web Development.</h4>
                         <h4 style={{ color: '#154360', opacity: '70%', fontSize: '15px'}}>I enjoy being a Husband, Dad, Executive Chef and Outdoorsmen</h4>
@@ -29,7 +33,7 @@ function HomePg() {
                         <a style={{ margin: '20px'}} href="https://www.linkedin.com/in/adam-stevenson-sea/" class='btn btn-dark' target='_blank' rel="noreferrer" id='button'><i className="fab fa-linkedin"></i> LinkedIn</a>
 
                         <a style={{ margin: '20px'}} href="https://www.instagram.com/adamsthecook/" class='btn btn-dark' target='_blank' rel="noreferrer" id='button'><i
-                            className="fab fa-github">Instagram</i></a>
+                            className="fab fa-instagram">Instagram</i></a>
                         
                         <a style={{ margin: '20px'}} href="https://github.com/adams1971" class='btn btn-dark' target='_blank' rel="noreferrer" id='button'><i
                             className="fab fa-github"></i></a>
